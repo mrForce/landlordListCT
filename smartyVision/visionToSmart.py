2adbc839d78b6ee30d74824104726e735b7f314c
Jordan Force <jordan.force@uconn.edu>
2021-04-23 18:40:49 -0400

    Need to finish implementing the Parcel class.

--- smartyVision/visionToSmart.py
+++ smartyVision/visionToSmart.py
@@ -50,8 +50,8 @@ class Parcel:
         assert(len(self.streets) == len(self.rows))
         self.inputIDList = list(range(base, base + len(self.streets)))
         return base + len(self.streets)
-    def getLookup(self):
-        #return a Lookup object. Split on the street to remove any unit numbers in the location. Use the MBLU to provide secondary. 
+    def getLookups(self):
+        #return a list of Lookup objects. Split on the street to remove any unit numbers in the location. Use the MBLU to provide secondary. 
         pass
     def setResult(self, inputID, result):
         """
@@ -63,7 +63,6 @@ class Parcel:
         4) Multiple lookups returned valid candidates, but they are inconsistent with one another. Signal to the user that this is the case. 
         """
         pass
-Parcel = collections.namedtuple('Parcel', ['mblu', 'location', 'streets'])
 parcels = {}
 with open(args.inputTSV, 'r') as f:
     reader = csv.DictReader(f, delimiter='\t')
@@ -78,22 +77,51 @@ with open(args.inputTSV, 'r') as f:
             if pid in parcels:
                 assert(mblu.strip() == parcels[pid].mblu)
                 assert(location.strip() == parcels[pid].location)
-                parcels[pid].streets.add(street)
+                parcels[pid].addStreet(street, row)
             else:
-                parcels[pid] = Parcel(mblu, location, set([street]))
+                parcels[pid] = Parcel(pid, mblu, location, street, row)
 
-parcelLocationCounter = collections.Counter()
+base = 0
+for pid, parcel in parcels.items():
+    base = percels[pid].assignInputIDs(base)
 
-
-batchSize = min(100, len(streets))
-
-recordIter = itertools.zip_longest(*streets.values())
-
+lookups = []
+batchsize=100
+#in case a parcel pushes us over the edge. 
+remainder = []
 creds = StaticCredentials('87f20fb5-a479-c2a6-61fc-f97766549421', 'aPjLRYlSqap2Q30bO5eM')
-
 client = ClientBuilder(creds).build_us_street_api_client()
 
+outputFile = open(args.output, 'w')
 
+fieldnames = visionHeaders + ['location_valid', 'city', 'zip', 'plus4_code', 'delivery_point', 'location_components', 'location_footnotes', 'location_metadata', 'location_footnote_Csharp', 'location_footnote_Dsharp', 'location_footnote_Fsharp', 'location_footnote_Hsharp', 'location_footnote_Isharp', 'location_footnote_Ssharp', 'location_footnote_Vsharp', 'location_footnote_Wsharp']
+
+footnoteMap = [('location_footnote_Csharp', 'C#'), ('location_footnote_Dsharp', 'D#'), ('location_footnote_Fsharp', 'F#'), ('location_footnote_Hsharp', 'H#'), ('location_footnote_Isharp', 'I#'), ('location_footnote_Ssharp', 'S#'), ('location_footnote_Vsharp', 'V#'), ('location_footnote_Wsharp', 'W#')]
+
+lookupIter = itertools.chain.from_iterable(map(parcel.items(), lambda pid, parcel: [(pid, lookup) for lookup in parcel.getLookups()]))
+for lookupSlice in itertools.islice(lookupIter, 100):
+    lookupList = list(lookupSlice)
+    batch = Batch()
+    for pid, lookup in lookupList:
+        batch.add(lookup)
+    assert(len(batch) == len(lookupList))
+    try:
+        client.send_batch(batch)
+    except exceptions.SmartyException as err:
+        print(err)
+        assert(False)
+    for i, lookup in enumerate(batch):
+        pid = lookupList[i][0]
+        parcels[pid].setResult(lookup.input_id, lookup.result)
+    
+for pid, parcel in parcels.items():
+    if len(batch) == 0 and len(remainder) > 
+    pLookups = parcel.getLookups()
+    i = 0
+    while i < len(pLookups) and len(batch) < batchsize:
+        batch.add(pLookups[i])
+    if i < len(pLookups):
+        
 
 class MBLU:
     def __init__(self, string):
